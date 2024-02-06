e85868e44c81a231af2c0773c1f2be996ef8a097
HoonJae <rlagnswo0505@naver.com>
2024-02-06 19:59:21 +0900

    디렉토리 구조 및 레이아웃 정리

---
+++ src/app/(afterLogin)/home/layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomeLayout = async ({ children }: Promise<Element>) => {
+  return <div>{children}</div>;
+};
+
+export default HomeLayout;
---
+++ src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const layout = ({ children }: any) => {
+  return (
+    <div>
+      애프터로그인 레이아웃
+      {children}
+    </div>
+  );
+};
+
+export default layout;
